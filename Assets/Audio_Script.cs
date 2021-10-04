25f90f9e83adccefc812c2d95e25795ec9722911
Nathan <nwholmes@knights.ucf.edu>
2021-10-03 23:13:48 -0400

    Animation commit

--- Assets/Audio_Script.cs
+++ Assets/Audio_Script.cs
@@ -10,32 +10,60 @@ public class Audio_Script : MonoBehaviour
 
     public AudioSource musicSource;
 
+    Animator anim;
+
     void Start()
+
     {
 
+        anim = GetComponent<Animator>();
+
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
         
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+
+        {
+
+            musicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+
+        {
+
+            musicSource.loop = false;
+        }
+
     }
 }
